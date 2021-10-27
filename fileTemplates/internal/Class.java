c97e32a9a7fddd5577ce8f1420b96f1ad62801a1
heiwa233 <2655318140@qq.com>
2021-10-27 22:11:28 +0800

    IU-2021.2.1 <zhangzhou@MCCdeAir Create baseRefactoring.xml, ide.general.xml, project.default.xml, rainbow_brackets.xml, Default.xml, yiiguxing.translation.xml, shared-indexes.xml, visualizationTool.xml, filetypes.xml, projectView.xml, qaPlugPluginUpdateNotifier.xml, mapper.xml, colors.scheme.xml, KeyPromoterXSettings.xml, databaseDrivers.xml, github.xml, laf.xml, qaplug_config.xml, Default.xml, Class.java, jdk.table.xml, vcs.xml, codota.xml, databaseSettings.xml, grazie_global.xml, toStringTemplates.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @description：TODO
+ * @author     ：猫丞丞 软件19-3 SVTCC
+ * @date       ：${DATE} ${TIME}
+ */
+public class ${NAME} {
+}
